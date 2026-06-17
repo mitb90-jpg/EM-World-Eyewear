8fa28374dc96258225b0ab094c5b7a2420f0fc06
MiTahzeeb <mitb90@gmail.com>
2026-06-17 18:30:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -233,10 +233,12 @@ if page == "👥 Clients":
 
     if clients:
 
-        delete_client_name = st.selectbox(
-            "Select Client",
-            ["Select Client"] + clients
-        )
+    delete_client_name = st.selectbox(
+        "Select Client",
+        options=["Select Client"] + clients,
+        index=0,
+        key="delete_client_dropdown"
+    )
 
 
         # DELETE CONFIRMATION
